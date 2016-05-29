5e8520ac7fd8235bd13772f310f28ee63012b02f
melbehi <montassarelbehi@gmail.com>
2016-05-29 20:22:18 +0100

    organize projects version + add junit test + add joda-time dep + organize packaging as enums and service

---
+++ e-gov-client/src/test/java/gov/esprit/client/test/UserTest.java
@@ -0,0 +1,28 @@
+package gov.esprit.client.test;
+import static org.junit.Assert.*;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.junit.Test;
+
+import gov.esprit.domain.Citoyen;
+import gov.esprit.service.UserServiceRemote;
+
+public class UserTest {
+
+	@Test
+	public void test() throws NamingException {
+		Context context = new InitialContext();
+		UserServiceRemote proxy = (UserServiceRemote) context
+			.lookup("e-gov-ear/e-gov-ejb/UserService!gov.esprit.service.UserServiceRemote");
+
+		Citoyen user = new Citoyen();
+		user.setNom("test");
+		proxy.addUser(user);
+			
+		assertEquals(1, proxy.findAll().size());
+	}
+
+}
