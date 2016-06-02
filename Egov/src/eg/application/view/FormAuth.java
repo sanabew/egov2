d56618444202b353e0de960dd180cb8b41946432
med <med_hadj@hotmail>
2016-06-02 21:29:20 +0200

    add client fx

---
+++ Egov/src/eg/application/view/FormAuth.java
@@ -0,0 +1,72 @@
+package eg.application.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+import javax.naming.NamingException;
+
+
+import eg.application.MainApp;
+
+
+
+public class FormAuth {
+
+	
+	@FXML
+    private TextField 
+    login;
+	@FXML
+    private TextField
+    password;
+	@FXML
+	private Button submit_auth;
+    // Reference to the main application.
+	/**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+
+	/**
+	 * The constructor (is called before the initialize()-method).
+	 * @throws NamingException 
+	 */
+	public FormAuth() throws NamingException {
+
+	}
+	
+	/**
+	 * Initializes the controller class. This method is automatically called
+	 * after the fxml file has been loaded.
+	 */
+	@FXML
+	private void initialize() {
+
+		
+		submit_auth.setOnAction((event) -> {
+			
+			try {
+				MainApp.primaryStage.setTitle("Espace User");
+				FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("view/ValidateNewChild.fxml"));
+				AnchorPane page = (AnchorPane) loader.load();
+				Scene scene = new Scene(page);
+				MainApp.primaryStage.setScene(scene);
+				MainApp.primaryStage.show();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	
+		});
+		
+		
+		
+	
+	}
+	
+}
